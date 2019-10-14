c2e3c690c6876e002f03af6bb3913839b9095b68
KevinLiebergen <kevinliebergen@hotmail.com>
2019-10-14 22:24:07 +0200

    Second commit

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
 import argparse
-import time
-import progressbar
+import progressbar as pb
+import gnupg
 import os
 
-def gpg_brute(ifile, wordlist):
-    with open(wordlist, 'r') as f:
 
-        for passw in f:
-            time.sleep(1)
-
-            passw = passw.strip()
-
-            command = 'gpg --batch --passphrase %s -d %s' % (passw, ifile)
-            
-            result = os.popen(command).read()
-
-            value_result = result.find("descifrado fallido")
-
-            print(value_result)
-            
-            if (value_result == -1):
-                #print(result)
-                print(passw)
-                print("Encontrado!!!")
-                break
+class color:    
+    green = '\033[92m'
+    END = '\033[0m'
 
 
-#    for i in progressbar.progressbar(range(100)):
-    #        time.sleep(0.02)
-
-
-
-def main():
+def call_arguments():
     parser = argparse.ArgumentParser(
             description="Especifica fichero y diccionario")
 
@@ -44,7 +22,61 @@ def main():
     parser.add_argument('--wordlist', required=True, 
             help="Ruta del diccionario a utilizar")
 
-    args = parser.parse_args()
+    return parser.parse_args()
+
+
+def cabecera():
+
+    print('''
+       ___  ___   ___    ___                 _     _____           _ 
+      / __|| _ \ / __|  / __| _ _  __ _  __ | |__ |_   _|___  ___ | |
+     | (_ ||  _/| (_ | | (__ | '_|/ _` |/ _|| / /   | | / _ \/ _ \| |
+      \___||_|   \___|  \___||_|  \__,_|\__||_\_\   |_| \___/\___/|_|
+                                        Made with <3 by KevinLiebergen
+        ''')
+
+outputfile = 'file_decrypted'
+
+def decrypt_gpg(password, encrypted_path, gpg):
+
+    with open(encrypted_path,'rb') as encrypt:
+
+        return  gpg.decrypt_file(encrypt, passphrase=password, output=outputfile) 
+
+
+def gpg_brute(ifile, wordlist):
+
+    gpg = gnupg.GPG()
+
+    i=0
+    nlines = len(open(wordlist).readlines())
+    
+    #initialize widgets
+    widgets = ['Time for loop of %s  iterations: '% nlines, pb.Percentage(), ' ',
+            pb.Bar(marker=pb.RotatingMarker()), ' ', pb.ETA()]
+    timer = pb.ProgressBar(widgets=widgets, maxval=nlines).start()
+
+    with open(wordlist, 'r') as f:
+
+        for passw in f:
+
+            passw = passw.strip()
+            result = decrypt_gpg(passw, ifile, gpg)
+    
+            if result.ok:
+                print(color.green + "\n[*] ENCONTRADO! La contraseña es: %s" % passw , color.END)
+                print("Fichero guardado en %s " % outputfile)
+                os._exit(1)
+            i += 1
+            timer.update(i)
+
+
+
+def main():
+   
+    args = call_arguments()
+
+    cabecera()
 
     gpg_brute(args.file, args.wordlist)
     
